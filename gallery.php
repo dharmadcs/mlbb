18bd90dd1116ffed1eb8ab3a69afe4b128a81f03
dharmadcs <dharmajiwork@gmail.com>
2022-07-13 14:59:41 +0700

    initial commit

---
+++ gallery.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>WEB - DARMAJI</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+  <div id="wrap">
+    <div id="header">
+    </div><!--header-->
+    
+    <audio hidden autoplay loop>
+    <source src="audio/m2-music.mp3" type="audio/mpeg">
+    Aktifkan Autoplay Di Browsermu
+  </audio>
+    
+    <div id="menu">
+    <ul>
+    <li><a href="index.php"><span><strong>HOME</strong></span></a></li>
+    <li><a href="heroes.php"><span><strong>HEROES</strong></span></a></li>
+    <li><a href="gallery.php"><span><strong>GALLERY</strong></span></a></li>
+    <li><a href="download.php"><span><strong>DOWNLOAD</strong></span></a></li>
+    <li><a href="services.php"><span><strong>SERVICES</strong></span></a></li>
+    </ul>
+    </div><!--menu-->
+
+    <div id="left">
+      <div id="top-left">
+        <div id="title">
+          <div align="center"><strong>SEASON REWARD</strong></div>
+        </div><!--title-->
+        <div id="content-tl">
+        <ol>
+        	<li>
+              <div align="left"><strong>WARIOR:	</strong> 1 x Fragment Skin Premium, 1000 Battle Point, dan 100 Ticket. </div>
+        	</li><br />
+        	<li>
+        	  <div align="left"><strong>ELITE: 	</strong>3x Fragment Skin Premium, 2000 Battle Point, dan 200 Ticket. </div>
+        	</li><br />
+            <li>
+              <div align="left"><strong>MASTER:</strong> Skin Eksklusif Season, 4000 Battle Point, dan 300 Ticket. </div>
+          </li><br />
+            <li>
+              <div align="left"><strong>GRANDMASTER:</strong> Skin Eksklusif Season, 7000 Battle Point, dan 600 Ticket. </div>
+          </li><br />
+            <li>
+              <div align="left"><strong>EPIC:  </strong>Skin Eksklusif Season, 12000 Battle Point, dan 1000 Ticket. </div>
+          </li><br />
+            <li>
+              <div align="left"><strong>LEGEND:</strong> Skin Eksklusif Season, 20000 Battle Point, dan 1500 Ticket. </div>
+          </li><br />
+            <li>
+              <div align="left"><strong>MYTHIC:</strong> Skin Ekslusif Season, Expression Mythic, 20000 Battle Point, dan 1500 Ticket.</div>
+          </li>
+        </ol>
+        </div><!--content-tl-->
+      </div><!--top-left-->
+      
+      <div id="top-left">
+        <div id="title">
+          <div align="center"><strong>ROLE</strong></div>
+        </div><!--title-->
+        <div id="content-tl">
+            <ol>
+                <li><strong>MARKSMAN</strong></li>
+                <li><strong>TANK</strong></li>
+                <li><strong>MAGE</strong></li>
+                <li><strong>FIGHTER</strong></li>
+                <li><strong>ASSASIN</strong></li>
+                <li><strong>SUPPORT</strong></li>
+            </ol>
+        </div><!--content-tl-->
+      </div><!--top-left-->
+      
+      <div id="top-left">
+        <div id="title">
+          <div align="center"><strong>PATCH NOTE</strong></div>
+        </div><!--title-->
+        <div id="content-tl">
+          <div align="center">Patch Notes 1.5.46 | 2021-01-12</div>
+          
+<ul>        
+<li>New Hero! The Heavently Fist</li>
+<li>Paquito coming to Original Server on 15th January.</li>
+<li>New Skins arrived: Clint's M2 Skin - Shadow Omen!</li>
+<li>New Event: M2 World Championship!</li>
+<li>27+ Bug was Fixed!</li>
+<li>Adjustments in Fragments!</li>
+
+</ul> 
+        </div><!--content-tl-->
+      </div><!--top-left-->
+    </div><!--left-->
+
+
+    <div id="right">
+      <div id="top-right">
+    	<div id="title">
+    	  <div align="center"> <marquee direction="left" scrollamount="1" align="center" behavior="alternate">
+    	  <em><strong>-- KUMPULAN SKIN ZODIAC MOBILE LEGENDS: BANG BANG --</strong></em>
+    	  </marquee></div>
+    	</div><!--title-->
+        <div id="content-tr">
+<marquee align="left" bgcolor="ecf0f1" direction="left" height="auto" scrollamount="2" behavior="alternate">
+
+
+<img src="img/skin_zodiac/cancer.webp" width="450px" height="280px"/>
+
+<img src="img/skin_zodiac/leo.webp" width="450px" height="280px"/>
+
+<img src="img/skin_zodiac/virgo.webp" width="450px" height="280px"/>
+
+<img src="img/skin_zodiac/libra.webp" width="450px" height="280px"/>
+
+<img src="img/skin_zodiac/scorpio.webp" width="450px" height="280px"/>
+
+<img src="img/skin_zodiac/sagitarius.webp" width="450px" height="280px"/>
+
+<img src="img/skin_zodiac/capricorn.webp" width="450px" height="280px"/>
+
+<img src="img/skin_zodiac/aquarius.webp" width="450px" height="280px"/>
+
+<img src="img/skin_zodiac/pisces.webp" width="450px" height="280px"/>
+
+</marquee> 
+        </div><!--content-tr-->
+      </div><!--top-right-->
+      
+      <div id="banner">
+      <img src="img/banner.jpg" />
+      </div><!--banner-->
+      
+      <div id="midle-right">
+      	<div id="mr-left">
+      		<div id="title">
+      		  <div align="center"><strong>EQUIPMENT</strong></div>
+      		</div><!--title-->
+            <div id="content-mr-left">
+            <div align="center">
+            <br/>
+            <strong><u>EQUIPMENT DIBAGI :</u></strong>
+            </div>
+            <ul type="square">
+          <li><strong>ATTACK</strong>: </li>
+                <li><strong>MAGIC</strong></li>
+                <li><strong>DEFENSE</strong></li>
+                <li><strong>MOVEMENT</strong></li>
+                <li><strong>JUNGLING</strong></li>
+                <li><strong>ROAMING</strong></li>
+            </ul>
+          </div><!--content-mr-left-->
+        </div><!--mr-left-->
+        
+        <div id="mr-center">
+      		<div id="title">
+      		  <div align="center"><strong>EMBLEM</strong></div>
+      		</div><!--title-->
+            <div id="content-mr-left">
+            <div align="center">
+            <br/>
+            <strong><u>EMBLEM DIBAGI :</u></strong>
+            </div>
+            <ul type="circle">
+              <li><strong>PHYSICAL</strong>: </li>
+                <li><strong>MAGICAL</strong></li>
+                <li><strong>TANK</strong></li>
+                <li><strong>JUNGLE</strong></li>
+                <li><strong>ASSASIN</strong></li>
+                <li><strong>MAGE</strong></li>
+                <li><strong>FIGHTER</strong></li>
+                <li><strong>MARKSMAN</strong></li>
+                <li><strong>SUPPORT</strong></li>
+            </ul>
+            </div><!--content-mr-left-->
+        </div><!--mr-center-->
+        
+        <div id="mr-right">
+      		<div id="title">
+      		  <div align="center"><strong>SPELL</strong></div>
+      		</div><!--title-->
+            <div id="content-mr-left">
+            <div align="center">
+            <br/>
+            <strong><u>SPELL DIBAGI :</u></strong>
+            </div>
+             <ul type="disc">
+               <li><strong>EXECUTE</strong>: </li>
+                <li><strong>RETRIBUTION</strong></li>
+                <li><strong>INSPIRE</strong></li>
+                <li><strong>SPRINT</strong></li>
+                <li><strong>HEALING</strong></li>
+                <li><strong>AEGIS</strong></li>
+                <li><strong>PETRIFY</strong></li>
+                <li><strong>PURIFY</strong></li>
+                <li><strong>FLICKER</strong></li>
+                <li><strong>ARRIVAL</strong></li>
+            </ul>
+            </div><!--content-mr-left-->
+        </div><!--mr-right-->
+        
+      </div><!--midle-right-->
+      
+      <div id="bottom-right">
+      	<div id="br-left">
+        	<div id="title">
+        	  <div align="center"><strong>PERTANDINGAN INTERNASIONAL : M2</strong></div>
+        	</div>
+            <div id="content-br-left">
+<marquee direction="up" scrollamount="2" height="100%">
+		<h2 align="center">&quot; AKTIFKAN AUTOPLAY BROWSERMU UNTUK MENDENGARKAN MUSIC M2 &quot;</h2>
+        <h3 align="left">&nbsp;</h3>
+        <h3 align="left">Pembagian Grup M2 Mobile Legends World Championship</h3>
+        <p align="left">Terdapat beberapa Negara yang mengikuti turnamen M2 Mobile Legends   World Championship diantaranya yaitu Indonesia, Malaysia, Singapura,   Filipina, Myanmar, Jepang, Brazil, Kamboja dan Rusia.</p>
+        <p align="left"><strong>Daftar Grup :</strong></p>
+        <p align="left"><strong>Grup A</strong></p>
+        <div align="left">
+                  <ul>
+                    <li>Burmese Ghouls (Myanmar)</li>
+                    <li>RSG (Singapura)</li>
+                    <li>Unique Dejavu (Rusia)</li>
+                  </ul>
+        </div>
+        <p align="left"><strong>Grup B</strong></p>
+        <div align="left">
+                  <ul>
+                    <li>Todak Mobile Legend (Malaysia)</li>
+                    <li>Omge Esports (Filipina)</li>
+                    <li>Impunity Cambodia (Kamboja)</li>
+                  </ul>
+        </div>
+        <p align="left"><strong>Grup C</strong></p>
+        <div align="left">
+                  <ul>
+                    <li>Bren Esports (Filipina)</li>
+                    <li>Alter Ego Esports (Indonesia)</li>
+                    <li>10second Gaming Frost (Jepang)</li>
+                  </ul>
+        </div>
+        <p align="left"><strong>Grup D</strong></p>
+        <div align="left">
+                  <ul>
+                    <li>RRQ Hoshi (Indonesia)</li>
+                    <li>EVOS SG MY (Singapura)</li>
+                    <li>DreamMax E-sports (Brazil)</li>
+                  </ul>
+        </div>
+        <p align="left">Dilansir dari dailyspin, sistem bracket M2 akan dilaksanakan dengan   sistem pertandingan BO3. Untuk juara grup akan langsung ke upper bracket   di babak playoff. Sedangkan team yang menduduki peringkat ke 2 dan 3   tidak akan langsung gugur begitu saja.</p>
+        <p align="left">Mereka akan bermain dengan memperebutkan slot di lower bracket   playoffs. Pertandingan fase group akan dilaksanakan pada 18 – 20 Januari   2020 dan babak playoffs dilaksanakan pada 22 – 24 Januari 2020.</p>
+        <p align="left">12 team yang bertanding akan memperebutkan total hadiah senilai $300 atau kurang lebih setara dengan 4,5 Miliar Rupiah.</p>
+        <p align="left">Pertandingan dapat kalian saksikan secara online, live di channel   Youtube dan Facebook MPL Indonesia. Sedangkan pertandingan offline akan   dilaksanakan di Hotel Shangri-La, Singapura.</p>
+        <p align="left">M2 merupakan kompetisi Mobile Legends berkala dunia yang diadakan   untuk kedua kalinya. Sebelumnya M1 sudah diadakan di Malaysia dengan   EVOS Legends sebagai juara setelah mengalahkan RRQ Hoshi di Final.</p>
+        <p align="left">Namun karena kekalahan yang harus diterima oleh EVOS Legend di MPL   Season 6, membuat mereka tidak bisa masuk ke M2 Mobile Legends untuk   mempertahankan gelar juara. Kali ini, EVOS Legends harus rela   mempercayakan M2 kepada rival mereka yaitu RRQ Hoshi dan Alter Ego   Esports.</p>
+        <p align="left">Mari kita tunggu saja pertandingan bergengsi ini pada Januari 2020</a> mendatang. Kira-kira siapa yang akan menjadi juaranya? Apakah RRQ Hoshi   kali ini bisa merebut gelar juara di M2 Mobile Legends World   Championship?</p>
+        <p align="left">&nbsp;</p></marquee>
+            </div>
+            
+        </div><!--br-left-->
+            
+        <div id="br-right">
+        <img src="img/m2.jpg"
+        </div><!--br-right-->
+            
+      </div><!--bottom-right-->
+      
+    </div><!--right-->
+
+    <div id="footer">
+    <div id="footer-ct">
+        <div id="title">
+          <div align="center"><strong>BEST QUOTES HERO</strong></div>
+        </div><!--title-->
+        
+        <div id="content-tl">
+        </div><!--content-tl-->
+      	<marquee bgcolor="#ecf0f1"; scrollamount="3">
+      	<strong> " Gatot Kaca - We are unity in diversity. (Bhinneka tunggal ika). "</strong>
+      	</marquee>
+        <strong>
+        <marquee bgcolor="#7f8c8d"; scrollamount="2">
+        " Fanny - War is not a game. (Perang bukanlah permainan). "
+        </marquee>
+        <marquee bgcolor="#ecf0f1"; scrollamount="3">
+        " Moskov - Everyone changes, so do I. (Semua orang berubah, begitu juga aku)."
+        </marquee>
+        <marquee bgcolor="#7f8c8d"; scrollamount="2">
+        " Alpha - Why must people, Hurt each other? (Kenapa manusia harus saling menyakiti). "
+        </marquee>
+      </strong></div><!--footer-->
+
+  </div><!--wrap-->
+</body>
+</html>
